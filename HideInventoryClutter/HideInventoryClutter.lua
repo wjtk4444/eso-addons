57dd3f890fd59f402dcb878f896a92132c6bc793
wjtk4444 <wjtk4444@gmail.com>
2022-11-24 12:50:02 +0100

    HideInventoryClutter - added deconstruction and vendor filters

--- HideInventoryClutter/HideInventoryClutter.lua
+++ HideInventoryClutter/HideInventoryClutter.lua
@@ -1,29 +1,37 @@
 HideInventoryClutter = { 
-	name = "HideInventoryClutter"
+	ADDON_NAME = "HideInventoryClutter",
+	log = true  and function(msg) d("[HideInventoryClutter]: "        .. msg) end or function() end,
+	dbg = true  and function(msg) d("[HideInventoryClutter][DEBUG]: " .. msg) end or function() end,
+	err = true  and function(msg) d("[HideInventoryClutter][ERROR]: " .. msg) end or function() end,
 }
 
+function HideInventoryClutter:isCraftingMaterial(slot) 
+	if not self.savedVariables.materialsEnabled then return false end
+	return GetItemFilterTypeInfo(slot.bagId, slot.slotIndex) == ITEMFILTERTYPE_CRAFTING 
+		or GetItemType(slot.bagId, slot.slotIndex) == ITEMTYPE_LURE
+end
+
+function HideInventoryClutter:isWorthDeconstructing(b, i)
+	if not self.savedVariables.deconstructionEnabled then return true end
+
+	local itemType = GetItemType(b, i)
+	if itemType == ITEMTYPE_GLYPH_ARMOR or itemType == ITEMTYPE_GLYPH_JEWELRY or itemType == ITEMTYPE_GLYPH_WEAPON then
+		return not IsItemLinkCrafted(GetItemItemLink(b, i))
+	end
+	
+	local quality = GetItemQuality(b, i)
+	if GetItemFilterTypeInfo(b, i) == ITEMFILTERTYPE_JEWELRY then 
+		return quality >= ITEM_QUALITY_MAGIC
+	end
+
+	return quality >= ITEM_QUALITY_ARTIFACT
+end
+
 function HideInventoryClutter:isConsumable(slot)
+	if not self.savedVariables.consumablesEnabled then return false end
+	
 	if IsItemStolen(slot.bagId, slot.slotIndex) then return false end
 
-	if not self.EXTRA_CONSUMABLES then
-		self.EXTRA_CONSUMABLES = {
-			[30357 ] = true, -- Lockpick
-			[44879 ] = true, -- Grand Repair Kit
-			[33271 ] = true, -- Soul Gem
-			--[64537 ] = true, -- Crown Experience Scroll, 50%
-			--[94441 ] = true, -- Grand Gold Coast Experience Scroll
-		}
-	end
-	
-	if not self.CONSUMABLE_ITEMTYPES then 
-		self.CONSUMABLE_ITEMTYPES = {
-			[ITEMTYPE_FOOD  ] = true,
-			[ITEMTYPE_DRINK ] = true,
-			[ITEMTYPE_POTION] = true,
-			[ITEMTYPE_POISON] = true,
-		}
-	end
-	
 	if self.EXTRA_CONSUMABLES[GetItemId(slot.bagId, slot.slotIndex)] then return true end
 	
 	local itemType = GetItemType(slot.bagId, slot.slotIndex) -- returns: itemType, specializedItemType
@@ -31,116 +39,23 @@ function HideInventoryClutter:isConsumable(slot)
 end
 
 function HideInventoryClutter:isLocked(slot)
+	if not self.savedVariables.lockedEnabled then return false end
 	return CanItemBePlayerLocked(slot.bagId, slot.slotIndex) and IsItemPlayerLocked(slot.bagId, slot.slotIndex)
 end
 
 --------------------------------------------------------------------------------
 
-function HideInventoryClutter:initialize()	
-	self.consumablesVisible = true
-	self.lockedVisible = true
-	self.movableIcon = false
-	self.bankOpen = false
-	self.storeOpen = false
-	self.libFilters = LibFilters3
-	self.libFilters:InitializeLibFilters()
-	
-	local savedVariables = ZO_SavedVars:NewAccountWide('HideInventoryClutterPosition', 1, nil, { 
-			positionConsumable = { top = 200, left = 200 },
-			positionLocked     = { top = 200, left = 300 },
-		})
-		
-	self.positionConsumable = savedVariables.positionConsumable
-	self.positionLocked     = savedVariables.positionLocked
-
-	HideInventoryClutter_ConsumablesButton:SetAlpha(1)
-	HideInventoryClutter_ConsumablesButton:ClearAnchors()
-	HideInventoryClutter_ConsumablesButton:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, 
-		self.positionConsumable.left, self.positionConsumable.top)
-	
-	HideInventoryClutter_LockedButton:SetAlpha(1)
-	HideInventoryClutter_LockedButton:ClearAnchors()
-	HideInventoryClutter_LockedButton:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, 
-		self.positionLocked.left, self.positionLocked.top)
-
-	self.LOCKED_FILTERS = {
-		[LF_BANK_DEPOSIT       ] = function(s) return not self:isLocked(s) end,
-		[LF_HOUSE_BANK_DEPOSIT ] = function(s) return not self:isLocked(s) end,
-		[LF_BANK_WITHDRAW      ] = function(s) return true end, -- temporary countermeasure to libFilters bug
-		[LF_HOUSE_BANK_WITHDRAW] = function(s) return true end, -- temporary countermeasure to libFilters bug
-		[LF_INVENTORY          ] = function(s) return not self:isLocked(s) end,
-	}
-	
-	self.CONSUMABLE_FILTERS = {
-		[LF_BANK_DEPOSIT       ] = function(s) return not self:isConsumable(s) end,
-		[LF_HOUSE_BANK_DEPOSIT ] = function(s) return not self:isConsumable(s) end,
-		[LF_BANK_WITHDRAW      ] = function(s) return true end, -- temporary countermeasure to libFilters bug
-		[LF_HOUSE_BANK_WITHDRAW] = function(s) return true end, -- temporary countermeasure to libFilters bug
-		[LF_VENDOR_SELL        ] = function(s) return not self:isConsumable(s) end,
-		[LF_MAIL_SEND          ] = function(s) return not self:isConsumable(s) end,
-		[LF_TRADE              ] = function(s) return not self:isConsumable(s) end,
-		[LF_INVENTORY          ] = function(s) return not self:isConsumable(s) end,
-	}
-		
-	local onShowPlayerInventory = ZO_PlayerInventory:GetHandler("OnShow")
-	ZO_PlayerInventory:SetHandler("OnShow", function(...)
-		HideInventoryClutter_ConsumablesButton:SetHidden(false)
-		HideInventoryClutter_LockedButton:SetHidden(false)
-
-		if onShowPlayerInventory then onShowPlayerInventory(...) end
-    end)
-	
-	local onHidePlayerInventory = ZO_PlayerInventory:GetHandler("OnHide")
-	ZO_PlayerInventory:SetHandler("OnHide", function(...)
-		if not (self.bankOpen or self.storeOpen) then
-			HideInventoryClutter_ConsumablesButton:SetHidden(true)
-			HideInventoryClutter_LockedButton:SetHidden(true)
-		end
-			
-		if onHidePlayerInventory then onHidePlayerInventory(...) end
-    end)	
-	
-	EVENT_MANAGER:RegisterForEvent(self.name, EVENT_OPEN_BANK, function()
-			self.bankOpen = true
-			HideInventoryClutter_ConsumablesButton:SetHidden(false)
-			HideInventoryClutter_LockedButton:SetHidden(false)
-		end)
-	EVENT_MANAGER:RegisterForEvent(self.name, EVENT_CLOSE_BANK, function()
-			self.bankOpen = false
-			HideInventoryClutter_ConsumablesButton:SetHidden(true)
-			HideInventoryClutter_LockedButton:SetHidden(true)
-		end)
-		
-	EVENT_MANAGER:RegisterForEvent(self.name, EVENT_OPEN_STORE, function()
-			self.storeOpen = true
-			HideInventoryClutter_ConsumablesButton:SetHidden(false)
-			HideInventoryClutter_LockedButton:SetHidden(false)
-		end)
-	EVENT_MANAGER:RegisterForEvent(self.name, EVENT_CLOSE_STORE, function()
-			self.storeOpen = false
-			HideInventoryClutter_ConsumablesButton:SetHidden(true)
-			HideInventoryClutter_LockedButton:SetHidden(true)
-		end)
-
-	self:toggleLocked()
-	self:toggleConsumables()
-	
-	SLASH_COMMANDS['/hideinventorycluttertogglemoveicons'] = function() 
-			self.movableIcon = not self.movableIcon
-			d('[HideInventoryClutter]: Icons ' .. (self.movableIcon and 'un' or '') .. 'locked!')
-			HideInventoryClutter_ConsumablesButton:SetMovable(self.movableIcon)
-			HideInventoryClutter_LockedButton:SetMovable(self.movableIcon)
-		end
-end
-
---------------------------------------------------------------------------------
-
 function HideInventoryClutter:toggleConsumables()
+	if self.movableIcon then
+		self.log("Toggle is disabled when icons are unlocked " .. (self.movableIcon and "un" or "") .. "locked!")
+		return
+	end
+
 	for menu, filter in pairs(self.CONSUMABLE_FILTERS) do
 		if self.consumablesVisible then
-			self.libFilters:RegisterFilter(self.name .. 'consumables' .. tostring(menu), menu, filter)
+			self.libFilters:RegisterFilter(self.ADDON_NAME .. "consumables" .. tostring(menu), menu, filter)
 		else
-			self.libFilters:UnregisterFilter(self.name .. 'consumables' .. tostring(menu), menu)
+			self.libFilters:UnregisterFilter(self.ADDON_NAME .. "consumables" .. tostring(menu), menu)
 		end
 		self.libFilters:RequestUpdate(menu)
 	end
@@ -150,11 +65,16 @@ function HideInventoryClutter:toggleConsumables()
 end
 
 function HideInventoryClutter:toggleLocked()
+	if self.movableIcon then
+		self.log("Toggle is disabled when icons are unlocked " .. (self.movableIcon and "un" or "") .. "locked!")
+		return
+	end
+
 	for menu, filter in pairs(self.LOCKED_FILTERS) do
 		if self.lockedVisible then		
-			self.libFilters:RegisterFilter(self.name .. 'locked' .. tostring(menu), menu, filter)
+			self.libFilters:RegisterFilter(self.ADDON_NAME .. "locked" .. tostring(menu), menu, filter)
 		else
-			self.libFilters:UnregisterFilter(self.name .. 'locked' .. tostring(menu), menu)
+			self.libFilters:UnregisterFilter(self.ADDON_NAME .. "locked" .. tostring(menu), menu)
 		end
 		self.libFilters:RequestUpdate(menu)
 	end
@@ -166,28 +86,241 @@ end
 --------------------------------------------------------------------------------
 
 function HideInventoryClutter:saveGuiPosition()
-	self.positionConsumable.left = math.floor(HideInventoryClutter_ConsumablesButton:GetLeft())
-	self.positionConsumable.top  = math.floor(HideInventoryClutter_ConsumablesButton:GetTop())	
+	self.savedVariables.positionConsumable.left = math.floor(HideInventoryClutter_ConsumablesButton:GetLeft())
+	self.savedVariables.positionConsumable.top  = math.floor(HideInventoryClutter_ConsumablesButton:GetTop())	
 	
-	self.positionLocked.left = math.floor(HideInventoryClutter_LockedButton:GetLeft())
-	self.positionLocked.top  = math.floor(HideInventoryClutter_LockedButton:GetTop())	
+	self.savedVariables.positionLocked.left = math.floor(HideInventoryClutter_LockedButton:GetLeft())
+	self.savedVariables.positionLocked.top  = math.floor(HideInventoryClutter_LockedButton:GetTop())	
 end
 
 function HideInventoryClutter:getTooltipTextClutter()
-	return "Consumables " .. (self.consumablesVisible and "visible" or "hidden") .. ".\n" ..
-		"Run '/hideinventorycluttertogglemoveicons' in chat to unlock/lock icon positions"
+	return "Consumables " .. (self.consumablesVisible and "visible" or "hidden")
 end
 
 function HideInventoryClutter:getTooltipTextLocked()
-	return "Locked items " .. (self.lockedVisible and "visible" or "hidden") .. ".\n" ..
-		"Run '/hideinventorycluttertogglemoveicons' in chat to unlock/lock icon positions"
+	return "Locked items " .. (self.lockedVisible and "visible" or "hidden")
 end
 
 --------------------------------------------------------------------------------
 
-EVENT_MANAGER:RegisterForEvent(HideInventoryClutter.name, EVENT_ADD_ON_LOADED, function() 
-        EVENT_MANAGER:UnregisterForEvent(HideInventoryClutter.name, EVENT_ADD_ON_LOADED)
-		HideInventoryClutter:initialize()
+function HideInventoryClutter:showButtons()
+	self.buttonsVisible = true
+	HideInventoryClutter_ConsumablesButton:SetHidden(false)
+	HideInventoryClutter_LockedButton:SetHidden(false)
+end
+
+function HideInventoryClutter:hideButtons()
+	self.buttonsVisible = false
+	HideInventoryClutter_ConsumablesButton:SetHidden(true)
+	HideInventoryClutter_LockedButton:SetHidden(true)
+end
+
+function HideInventoryClutter:initalize()
+	self.consumablesVisible = true
+	self.lockedVisible = true
+	self.movableIcon = false
+	self.buttonsVisible = false
+	self.libFilters = LibFilters3
+	self.libFilters:InitializeLibFilters()
+	
+	self.savedVariables = ZO_SavedVars:NewAccountWide("HideInventoryClutterPosition", 1, nil, { 
+			positionLocked     = { top = 820, left = 1361 },
+			positionConsumable = { top = 811, left = 1379 },
+			lockedEnabled = true,
+			consumablesEnabled = true,
+			materialsEnabled = true,
+			deconstructionEnabled = true,
+		})
+
+	HideInventoryClutter_ConsumablesButton:SetAlpha(1)
+	HideInventoryClutter_ConsumablesButton:ClearAnchors()
+	HideInventoryClutter_ConsumablesButton:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, 
+		self.savedVariables.positionConsumable.left, self.savedVariables.positionConsumable.top)
+	
+	HideInventoryClutter_LockedButton:SetAlpha(1)
+	HideInventoryClutter_LockedButton:ClearAnchors()
+	HideInventoryClutter_LockedButton:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, 
+		self.savedVariables.positionLocked.left, self.savedVariables.positionLocked.top)
+
+	self.CONSUMABLE_ITEMTYPES = {
+		[ITEMTYPE_FOOD  ] = true,
+		[ITEMTYPE_DRINK ] = true,
+		[ITEMTYPE_POTION] = true,
+		[ITEMTYPE_POISON] = true,
+	}
+
+	self.EXTRA_CONSUMABLES = {
+		[30357 ] = true, -- Lockpick
+		[44879 ] = true, -- Grand Repair Kit
+		[33271 ] = true, -- Soul Gem
+		[121550] = true, -- Scroll of Pelinal's Ferocity
+	}
+
+	self.LOCKED_FILTERS = {
+		[LF_BANK_DEPOSIT        ] = function(s) return not self:isLocked(s) end,
+		[LF_HOUSE_BANK_DEPOSIT  ] = function(s) return not self:isLocked(s) end,
+		[LF_BANK_WITHDRAW       ] = function(s) return true end, -- temporary countermeasure to libFilters bug
+		[LF_HOUSE_BANK_WITHDRAW ] = function(s) return true end, -- temporary countermeasure to libFilters bug
+		[LF_INVENTORY           ] = function(s) return not self:isLocked(s) end,
+		[LF_VENDOR_SELL         ] = function(s) 
+				if self.savedVariables.deconstructionEnabled then
+					return not self:isCraftingMaterial(s) and not self:isWorthDeconstructing(s.bagId, s.slotIndex)
+				else
+					return not self:isCraftingMaterial(s)
+				end
+			end,
+		[LF_SMITHING_DECONSTRUCT] = function(b, i) return self:isWorthDeconstructing(b, i) end,
+		[LF_JEWELRY_DECONSTRUCT ] = function(b, i) return self:isWorthDeconstructing(b, i) end,
+	}
+	
+	self.CONSUMABLE_FILTERS = {
+		[LF_BANK_DEPOSIT        ] = function(s) return not self:isConsumable(s) end,
+		[LF_HOUSE_BANK_DEPOSIT  ] = function(s) return not self:isConsumable(s) end,
+		[LF_GUILDBANK_WITHDRAW  ] = function(s) return not self:isConsumable(s) end,
+		[LF_GUILDBANK_DEPOSIT   ] = function(s) return not self:isConsumable(s) end,
+		[LF_BANK_WITHDRAW       ] = function(s) return true end, -- temporary countermeasure to libFilters bug
+		[LF_HOUSE_BANK_WITHDRAW ] = function(s) return true end, -- temporary countermeasure to libFilters bug
+		[LF_VENDOR_SELL         ] = function(s) return not self:isConsumable(s) end,
+		[LF_MAIL_SEND           ] = function(s) return not self:isConsumable(s) end,
+		[LF_TRADE               ] = function(s) return not self:isConsumable(s) end,
+		[LF_INVENTORY           ] = function(s) return not self:isConsumable(s) end,
+		[LF_GUILDSTORE_SELL     ] = function(s) return not self:isConsumable(s) end,
+	}
+
+	local onShowPlayerInventory = ZO_PlayerInventory:GetHandler("OnShow")
+	ZO_PlayerInventory:SetHandler("OnShow", function(...)
+		self:showButtons()
+		if onShowPlayerInventory then onShowPlayerInventory(...) end
     end)
+	
+	local onHidePlayerInventory = ZO_PlayerInventory:GetHandler("OnHide")
+	ZO_PlayerInventory:SetHandler("OnHide", function(...)
+		--if not self.buttonsVisible then 
+		self:hideButtons()
+		--end
+		if onHidePlayerInventory then onHidePlayerInventory(...) end
+    end)	
+	
+	EVENT_MANAGER:RegisterForEvent(self.ADDON_NAME, EVENT_OPEN_BANK,  function() self:showButtons() end)
+	EVENT_MANAGER:RegisterForEvent(self.ADDON_NAME, EVENT_CLOSE_BANK, function() self:hideButtons() end)
+		
+	EVENT_MANAGER:RegisterForEvent(self.ADDON_NAME, EVENT_OPEN_STORE,  function() self:showButtons() end)
+	EVENT_MANAGER:RegisterForEvent(self.ADDON_NAME, EVENT_CLOSE_STORE, function() self:hideButtons() end)
 
+	EVENT_MANAGER:RegisterForEvent(self.ADDON_NAME, EVENT_CRAFTING_STATION_INTERACT, function() self:showButtons() end)
+	EVENT_MANAGER:RegisterForEvent(self.ADDON_NAME, EVENT_END_CRAFTING_STATION_INTERACT, function() self:hideButtons() end)
+	
+	self:toggleLocked()
+	self:toggleConsumables()
+end
 
+function HideInventoryClutter:buildMenu()
+    local LAM2 = LibAddonMenu2
+	LAM2:RegisterAddonPanel(self.ADDON_NAME  .. "_LAM_PANEL", { type = "panel", name = "Hide Inventory Clutter" })
+	local controls = {
+		[1] = {
+			type = "description",
+			width = "full",
+			text = 
+[[An addon that hides inventory clutter, namely locked items and common consumbles.
+See available filters below (hover over them to get a short description).
+Check addon's website or see README.md file in addon folder to learn the details.
+Full list of affected consumables:
+- lockpicks
+- grand repair kits
+- soul gems
+- buff food / drinks
+- potions
+- poisons]]
+		},
+		[2] = {
+			type = "button",
+			name = "toggle lock / unlock icon position",
+			func = function() 
+				self.movableIcon = not self.movableIcon
+				self.log("Icons " .. (self.movableIcon and "un" or "") .. "locked!")
+				HideInventoryClutter_ConsumablesButton:SetMovable(self.movableIcon)
+				HideInventoryClutter_LockedButton:SetMovable(self.movableIcon)
+
+				if self.movableIcon then
+					HideInventoryClutter_LockedButton:SetAlpha(1)
+					HideInventoryClutter_LockedButton:SetHidden(false)
+					HideInventoryClutter_ConsumablesButton:SetAlpha(1)
+					HideInventoryClutter_ConsumablesButton:SetHidden(false)
+				else
+					HideInventoryClutter_LockedButton:SetAlpha(self.lockedVisible and 1 or 0.3)
+					HideInventoryClutter_LockedButton:SetHidden(true)
+					HideInventoryClutter_ConsumablesButton:SetAlpha(self.lockedVisible and 1 or 0.3)
+					HideInventoryClutter_ConsumablesButton:SetHidden(true)
+				end
+			end
+		},
+		[3] = {
+			type = "button",
+			name = "reset position",
+			func = function()
+				if not self.movableIcon then
+					self.log("Icons " .. (self.movableIcon and "un" or "") .. "locked!")
+				end
+
+				self.savedVariables.positionConsumable.top  = 811 
+				self.savedVariables.positionConsumable.left = 1379 
+				HideInventoryClutter_ConsumablesButton:ClearAnchors()
+				HideInventoryClutter_ConsumablesButton:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, 
+					self.savedVariables.positionConsumable.left, self.savedVariables.positionConsumable.top)
+				
+				self.savedVariables.positionLocked.top  = 820 	
+				self.savedVariables.positionLocked.left = 1361 
+				HideInventoryClutter_LockedButton:ClearAnchors()
+				HideInventoryClutter_LockedButton:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, 
+					self.savedVariables.positionLocked.left, self.savedVariables.positionLocked.top)
+
+				self:saveGuiPosition()
+			end
+		},
+		[4] = {
+			type = "checkbox",
+			name = "Locked items filter",
+			tooltip = "hides locked items from inventory window",
+			getFunc = function() return self.savedVariables.lockedEnabled end,
+			setFunc = function(value) 
+				self.savedVariables.lockedEnabled = value 
+				self:toggleLocked()
+				self:toggleLocked()
+			end
+		},
+		[5] = {
+			type = "checkbox",
+			name = "Consumables filter",
+			tooltip = "hides common consumables from inventory and merchant sell window",
+			getFunc = function() return self.savedVariables.consumablesEnabled end,
+			setFunc = function(value) 
+				self.savedVariables.consumablesEnabled = value
+				self:toggleConsumables()
+				self:toggleConsumables()
+			end
+		},
+		[6] = {
+			type = "checkbox",
+			name = "Materials filter",
+			tooltip = "hides materials from merchant sell window",
+			getFunc = function() return self.savedVariables.materialsEnabled end,
+			setFunc = function(value) self.savedVariables.materialsEnabled = value end
+		},
+		[7] = {
+			type = "checkbox",
+			name = "Deconstruction filter",
+			tooltip = "hides items not worth deconstructing from deconstruction window and items worth deconstucting from merchant window",
+			getFunc = function() return self.savedVariables.deconstructionEnabled end,
+			setFunc = function(value) self.savedVariables.deconstructionEnabled = value end
+		},
+	}
+	LAM2:RegisterOptionControls(self.ADDON_NAME .. "_LAM_PANEL", controls)
+end
+
+EVENT_MANAGER:RegisterForEvent(HideInventoryClutter.ADDON_NAME, EVENT_ADD_ON_LOADED, function(eventCode, addOnName)
+	if addOnName ~= HideInventoryClutter.ADDON_NAME then return end 
+	EVENT_MANAGER:UnregisterForEvent(HideInventoryClutter.ADDON_NAME, EVENT_ADD_ON_LOADED)
+	HideInventoryClutter:initalize()
+	HideInventoryClutter:buildMenu()
+end)
--- HideInventoryClutter/README.md
+++ HideInventoryClutter/README.md
@@ -1,14 +1,22 @@
 # HideInventoryClutter
 
-A libFilter3 based addon that adds a toggle to display/hide two categories of items from your inventory (as well as  depositing items to (house) bank, sending mails and selling to merchant):
+A libFilter3 based addon that adds a toggle to hide unnecessary inventory clutter from your sight (enabled by default after logging in). It's best used with [Personal Assistant](https://www.esoui.com/downloads/info381-PersonalAssistantBankingJunkLootRepair.html) configured to automatically mark and sell junk for you, which effectively hides everything you define as junk from your view.
 
-# locked items 
+# Getting started
 
-Most players cary multiple gear sets in their inventories, and obviously, they are aware of the fact. This addon removes visual clutter from player's inventory by toggling visibility of all locked items with 1 button click. (This filter is applied by default to mail and merchant menu, even turning it off will not allow you to sell or mail locked items.)
+This addon's UI consists of 2 clickable icons that can be placed anywhere on the screen. They become visible and active when you open inventory menu (or any other menu that displays your inventory's content, ie., bank or merchant). 
+To move icons to your desired position, open addon settings and unlock icons. Now you can open/close inventory and position them at will. Once done, simply open addon settings again and lock them again. 
+If you don't like one or more of the filters, they can all be disabled individually in the settings panel.
 
-# consumables
+# Locked items 
 
-Items that everyone carries at all times, seeing them is unnecessary unless restocking or selling excess amounts. Toggle applies to:
+Most players cary multiple gear sets in their inventories, and obviously, they are aware of the fact. Now you can toggle visibility of locked items with just one click. 
+
+_This filter is currently being unintentionally applied to bank/chest withdraw window as well. I am yet to find a workaround for this issue. Apologies for the inconvenience._
+
+# Consumables
+
+Items that everyone carries at all times, seeing them is unnecessary except for restocking or selling excess amounts. Toggle applies to:
 
 - Lockpick
 - Grand Repair Kit
@@ -18,14 +26,25 @@ Items that everyone carries at all times, seeing them is unnecessary unless rest
 - potions
 - poisons
 
-# Usage
+_This filter is currently being unintentionally applied to bank/chest withdraw window as well. I am yet to find a workaround for this issue. Apologies for the inconvenience._
 
-To move icons to the desired location on your screen use the following command, move them and use the command again.
+# Deconstruction filters
 
-`/hideinventorycluttertogglemoveicons`
+This addon determines whether something is worth deconstructing, ie., if materials gained from deconstruction are likely to be worth more than the amount of gold that merchant offers. Items that will appear in the deconstruction window by default meet those criteria:
+
+- Not a player-made enchant. While worth deconstructing, I assume that player-made enchants are meant to be used or sold in guild store in the near future.
+- Green or above quality jewelry. Terne Grains are not particularly expensive, but it's still worth more than selling an item to the merchant.
+- Purple or above quality gear. Blue and green upgrade materials are cheap, prices offered by merchant are superior. This check does not account for rare style materials that could be obtained by deconstructing items of lower quality. The reasoning behind this is simplicity, most of style materials aren't worth much and the rare ones are, well, rare.
+
+To toggle visibility in deconstruction menu use the **Lock** icon (that otherwise has no effect as locked items cannot be deconstructed without unlocking them first).
+
+# Merchant filters
+
+Items considered worth deconstructing, ie., purple gear (see full criteria above) will be hidden from the merchant sell window. Additionally, crafting materials are also hidden. This filter is targeted at people who decided to play the game without ESO+ membership and therefore carry materials in their inventory (before depositing them in bank. Once again my recommendation for automating all that goes to [Personal Assistant](https://www.esoui.com/downloads/info381-PersonalAssistantBankingJunkLootRepair.html). To toggle visibility in merchant sell window use the **Lock** icon (that otherwise has no effect as locked items cannot be sold before unlocking them first).
+
 
 # Known bugs
 
-Filters are currently being applied to (house) bank withdrawal panel. This is unintentional and hopefully (it's more related to LibFilters and ESO itself than my addon) will be fixed in the future, sorry for the inconvienience. If you want to withdraw locked/consumable items, you need to toggle the filter first.
+As mentioned before, inventory (locked items and consumables) filters are currently being applied to (house) bank withdrawal panel. This is unintentional and hopefully will be fixed in the future. If you want to withdraw locked/consumable items, you need to toggle the filter first.
 
 ![](../_screenshots/HideInventoryClutter/HideInventoryClutter.png)
